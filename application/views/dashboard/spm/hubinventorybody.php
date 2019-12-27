f53f70f27c5c3efe8266bb228822868a99fcf999
Aaron Angeles <selegna_8@yahoo.com.ph>
2019-12-27 17:24:02 +0800

    hubinv

--- application/views/dashboard/spm/hubinventorybody.php
+++ application/views/dashboard/spm/hubinventorybody.php
@@ -1,11 +1,34 @@
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">SP Mamplasan Hub Inventory Monitoring</h1>
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800">SP Mamplasan Hub Inventory Monitoring</h1>
 
+
+
+  <!-- Basic Card Example -->
+  <div class="card shadow mb-4 border-bottom-primary">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Add Item</h6>
+    </div>
+    <div class="card-body">
+      <form class="form-inline" method="post">
+        <label for="partno">Part No</label>
+        <div class="input-group" style="margin-left: 6px; margin-right: 6px;">
+          <input type="text" name="partno" id="partno" class="form-control">
         </div>
-        <!-- /.container-fluid -->
+        <button class="btn btn-success btn-icon-split">
+          <span class="icon text-white-50">
+            <i class="fas fa-check"></i>
+          </span>
+          <span class="text">Submit</span>
+        </button>
+      </form>
+    </div>
+  </div>
 
-      </div>
-      <!-- End of Main Content -->
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
